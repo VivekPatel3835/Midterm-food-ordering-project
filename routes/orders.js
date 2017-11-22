d3e9895dfeaaa94bed2dce73b4ddb1a3add0caaa
vagrant <lovemore_jokonya@yahoo.com>
2017-11-22 23:31:53 +0000

    added view and update single order routes

--- routes/orders.js
+++ routes/orders.js
@@ -30,14 +30,45 @@ module.exports = (knex) => {
   });
 
 
-  // Delete Order route
-  app.post("/orders/:order_id/delete", (req, res) => {
+  // Delete Order POST route
+  router.post("/orders/:order_id/delete", (req, res) => {
     //check if user is logged in => if not send err 500
     //define parameters from request.params
     //check if order_id exists in database => if not send err 404 status
     //delete order
     //redirect to view orders page
   });
+
+  // View Order GET route
+  router.get("/orders/:order_id", (req, res) => {
+    //check if user is logged in => if not send err 500
+    //define order_id from request.params
+    //check if order_id exists in database => if not send err 404 status
+    //ideally only the creator of the order should be able to view the order
+    //redirect to view order page or just do ajax request to display order details
+  });
+
+  // Update Order POST route
+  router.post("/orders/:order_id", (req, res) => {
+    //check if user is logged in => if not send err 500
+    //define parameters from request.params
+    //check if order_id exists in database => if not send err 404 status
+    //ideally only the creator of the order should be able to update the order
+    //update order
+    //redirect to view order page or just do ajax request to update current page
+  });
+
+  // Update Order GET route
+  router.get("/orders/:order_id/update", (req, res) => {
+    //check if user is logged in => if not send err 500
+    //define parameters from request.params
+    //check if order_id exists in database => if not send err 404 status
+    //ideally only the creator of the order should be able to update the order
+    //redirect to view order page or just do ajax request to display order details ready for editing
+  });
+
+
+
 
   return router;
 }
