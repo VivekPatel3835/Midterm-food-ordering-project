be695de30729f7991a20eb2d9df8b46a0eb85584
montygoldy <montygoldy2006@gmail.com>
2017-11-23 20:45:14 +0000

    Added more styles to homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,3 +19,17 @@ $('#bootstrap-touch-slider').bsTouchSlider();
     delay: 10,
     time: 2000
   });
+
+
+//Sticky header
+
+var navbar = $(".navbar");
+    stickyDiv = "sticky";
+
+$(window).scroll(function() {
+  if( $(this).scrollTop()) {
+    navbar.addClass(stickyDiv);
+  } else {
+    navbar.removeClass(stickyDiv);
+  }
+});
