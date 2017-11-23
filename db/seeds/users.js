8d2842edeaa40870adb98ae64b60f2438dcdf10a
VivekPatel3835 <patelvivek3835@gmail.com>
2017-11-23 16:33:55 +0000

    edited seed file

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,21 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, name: 'Alice', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Tony', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Arvind', email: 'john@doe.com', 'password'}),
+
+        knex('restaurants').insert({id: 1, name: 'Mcdonalds', email: 'john@doe.com', 'password'}),
+        knex('restaurants').insert({id: 1, name: 'Dominos', email: 'john@doe.com', 'password'}),
+        knex('restaurants').insert({id: 1, name: 'Arvind', email: 'john@doe.com', 'password'}),
+
+              knex('users').insert({id: 1, name: 'Alice', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Tony', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Arvind', email: 'john@doe.com', 'password'}),
+
+              knex('users').insert({id: 1, name: 'Alice', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Tony', email: 'john@doe.com', 'password'}),
+        knex('users').insert({id: 1, name: 'Arvind', email: 'john@doe.com', 'password'})
       ]);
     });
 };
