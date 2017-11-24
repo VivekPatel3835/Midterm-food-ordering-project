479997dc46536ad5574e3bce36d222651ae388ca
VivekPatel3835 <patelvivek3835@gmail.com>
2017-11-24 23:57:19 +0000

    pre-merge to master. items are being added to cart, but the join is duplicating each item. Bug not fixed yet.

--- routes/menu_items.js
+++ routes/menu_items.js
@@ -5,18 +5,44 @@ const router  = express.Router();
 
 module.exports = (knex) => {
   router.get("/", (req, res) => {
+    //this knex query prints the menu items for a particular restaurant
     knex
     .select("*")
     .from("menu_items")
     .then((results) => {
-      let cart = { quantity: 3, menu_items_id: '2', order_id: '1'};
-      console.log('in the menu_items routes file: results -> ', results, cart)
-        //res.json(results);
-        let params = {res: results,
-                      cart: cart}
-        res.render("../views/menu.ejs", params);
-      });
+      //this knex query retrieves the items in the corder cart
+      knex
+      .select('*')
+      .from('cart_items')
+      .where('order_id', '23')
+      .then((myCart) => {
+        //this knex query retrieves the menu_item_id from the cart-items
+        //then gets the actual details of the food items from the menu_items table
+        let subquery = knex.select('menu_items_id').from('cart_items').where('order_id', '23');
+        knex
+        .select('*')
+        .from('menu_items')
+        .whereIn('menu_items.id', subquery)
+        .leftOuterJoin('cart_items', function() {
+            this.on('order_id', '=', 23).on('menu_items_id', 'menu_items.id')
+        })
+        .then((myOrder) => {
+            console.log()
+            let params = {results: results,
+            myOrder: myOrder,
+            myCart : myCart
+            }
+            console.log(myOrder);
+            res.render("../views/menu.ejs", params);
+        })
+    })
   });
-      return router;
-    }
+});
+  return router;
+}
 
+// select * from menu_items where id in (select menu_items_id from cart_items where (order_id = 23));
+
+// knex.select('*').from('users').leftOuterJoin('accounts', function() {
+//   this.on('accounts.id', '=', 'users.account_id').orOn('accounts.owner_id', '=', 'users.id')
+// })
