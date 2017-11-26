c6c7c0e25acef517115c575e60d25021f22f76d4
vagrant <lovemore_jokonya@yahoo.com>
2017-11-26 15:06:42 +0000

    fixed menu_items not displaying bug - was not the same as actual master

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,52 +1,109 @@
-const printCartItems = () => {
-   // once you figure out how to maintian the same order id for the customer in the menu_items page,
-   //    come here and complete the get request to populate the cart-items sidebox
-          $.ajax({
+const printCartItems = (cart) => {
+  console.log(cart)
+  let html = ''
+  let totalPrice = 0
+  cart.forEach((item) => {
+    html += `<div class="row checkout_row">
+            <div class="col-md-12 inner_div">
+               <h4>${item.name}</h4>
+               <div class="edit_buttons">
+                  <a href="">Edit</a>
+                  <a href="">Delete</a>
+              </div>
+              <p><%= item.description %></p>
+              <h5>$ ${item.price}</h5>
+              <span class="quantity">Quantity: ${item.quantity}</span>
+          </div>
+      </div>
+    <!--   <div class="quantity_counter">
+    <span class="plus_button">
+    <button type="button" class="quantity-left-minus btn btn-number"  data-type="minus" data-field="">
+    <span class="glyphicon glyphicon-minus"></span>
+    </button>
+    </span>
+    <input type="text" name="quantity" class="form-control input-number quantity" value='<%= item.quantity%>' min="1" max="100"/>
+    <span class="minus_button">
+    <button type="button" class="quantity-right-plus btn btn-number" data-type="plus" data-field="">
+    <span class="glyphicon glyphicon-plus"></span>
+    </button>
+    </span>
+    </div> -->`
+    totalPrice += (item.price * item.quantity)
+})
+  html += `<span> Total:$ ${totalPrice}</span>`
+  $('#cartItemsContainer').html(html);
+}
+
+$(() => {
+    $.ajax({
         method: "GET",
-        url: "/menu_items",
-        success: () => {
-          console.log('in get /cart_items ajax request after creating the order log - now it should populate the carts box')
-        },
-        error: (error) => {
+        url: "/cart_items",
+        success: (cart) => {
+          console.log('in initial get /cart_items ajax request for menu_page load'
+            + 'created the order log - now it should populate the carts box')
+          printCartItems(cart)
+      },
+      error: (error) => {
           console.log('in get /cart_items ajax request - ajax call not working')
-        },
-        complete: () => {
+      },
+      complete: () => {
           console.log(' get /cart_items Ajax call complete. Does not mean that it was successfull')
-        }
-      });
-      console.log('in post /cart_items/cart ajax request')
+      }
+  });
+})
+
+const getCartItems = () => {
+   $.ajax({
+    method: "GET",
+    url: "/cart_items",
+    success: (cart) => {
+      console.log('in get /cart_items ajax request after '
+        + 'creating the order log - now it should populate the carts box')
+      printCartItems(cart)
+  },
+  error: (error) => {
+      console.log('in get /cart_items ajax request - ajax call not working')
+  },
+  complete: () => {
+      console.log(' get /cart_items Ajax call complete. Does not mean that it was successfull')
+  }
+});
 }
 
 $('body').on('click', '.menu_item', function() {
-  const data = {'special_message': 'not yet entered', 'status': 'cart-test', 'order_phone_number': 'not yet entered', 'menu_item_id': 1}
-  console.log('in cart.js ajax file - the item has been clicked ')
-    $.ajax({
+  const itemNumber = $(this).find('.menu_item_id').html();
+  const orderQuantity = $(this).find('.input-number').val();
+  const data = {'special_message': 'not yet entered',
+  'status': 'cart-test', 'order_phone_number': 'not yet entered',
+  'menuItemId': itemNumber, 'orderQuantity': orderQuantity}
+  console.log('in cart.js ajax file - the item has been clicked. order quantity --> ', orderQuantity)
+  $.ajax({
     method: "POST",
     data: data,
     url: "/cart_items",
     success: () => {
-      printCartItems();
+        getCartItems();
     },
     error: (error) => {
       console.log('ajax post - add item to cart not working')
-    },
-    complete: () => {
-      console.log('post /cart-items Ajax call complete. Does not mean that it was successfull')
-    }
-  });
+  },
+  complete: () => {
+    console.log('post /cart-items Ajax call complete. Does not mean that it was successfull')
+}
+});
 })
 
 
 //Sticky header
 
 var navbar = $(".navbar");
-    stickyDiv = "sticky";
+stickyDiv = "sticky";
 
 $(window).scroll(function() {
   if( $(this).scrollTop()) {
     navbar.addClass(stickyDiv);
-  } else {
+} else {
     navbar.removeClass(stickyDiv);
-  }
+}
 });
 
--- routes/menu_items.js
+++ routes/menu_items.js
@@ -10,61 +10,20 @@ module.exports = (knex) => {
     .select("*")
     .from('menu_items')
     .then((results) => {
-      // if(req.session.email === undefined) {
-      //   throw new Error(403)
-      // }
-      //this knex query gets the order id for the user that is logged in
-      let subqueryGetsUserId = knex.select('id').from('users').where('email', userEmail)
-      knex.select('id').from('order_logs').where('user_id', subqueryGetsUserId).first().then((order) => {
-            // console.log('orderID', order.id)
-           //this knex query retrieves the items in the order cart
-           knex
-           .select('*')
-           .from('cart_items')
-           .where('order_id', '=' , order.id)
-           .then((myCart) => {
-                // console.log('myCart -->', myCart)
-                //this knex query retrieves the menu_item_id from the cart-items
-                //then gets the actual details of the food items from the menu_items table
-                let getMenuItemIdsForOrder = knex.select('menu_items_id').from('cart_items').where('order_id', order.id);
-                knex
-                .select('*')
-                .from('menu_items')
-                .whereIn('menu_items.id', getMenuItemIdsForOrder)
-                .leftOuterJoin('cart_items', function() {
-                  this.on('order_id', '=', order.id).on('menu_items_id', 'menu_items.id')
-                })
-                .then((myOrder) => {
-                    // console.log('myOrder -->', myOrder)
-                  let params = {results: results,
-                    myOrder: myOrder,
-                    myCart : myCart,
-                    loggedInEmail: req.session.email
-                    }
-                    res.render("../views/menu.ejs", params);
-                })
-            })
-       })
-  })
+      let params = {
+        results: results,
+        loggedInEmail: userEmail
+      }
+      res.render("../views/menu.ejs", params);
+
+    })
     .catch((err) => {
       console.error(err)
       res.send('Please log in')
           // redirect to login or show message to login
-      })
-});
+        })
+  });
   return router;
 }
-
-// sql query:
-// select order_logs.id from order_logs where order_logs.user_id in
-// (select users.id from users where email in ('alice@doe.com'));
-
-// same query in knex:
-    // let subqueryGetsUserId = knex.select('id').from('users').where('email', req.session.email)
-    // knex.select('id').from('order_logs').where('user_id', subqueryGetsUserId).then((orderNumber) => {
-    //   console.log('myOrderNumber-->', orderNumber)
-    // })
-
-
 
 
