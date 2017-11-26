12598c57cd8951d687cf2836ae82d0281914516a
vagrant <lovemore_jokonya@yahoo.com>
2017-11-26 17:45:31 +0000

    removed unnecesary code and made explanatory comments

--- routes/send_sms.js
+++ routes/send_sms.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// const http = require('http');
 const express = require('express');
 const bodyParser = require("body-parser");
 require('dotenv').load();
@@ -14,26 +13,31 @@ const accountSid = process.env.ACCOUNT_SID;
 
 // require the Twilio module and create a REST client
 const client = require("twilio")(accountSid, authToken);
+// these phine numbers are defined in .env.
+// Substitute with actual numbers and phone number object submited by user in production environment.
 const fromNumber = process.env.FROM_NUMBER;
 const toNumber = process.env.TO_NUMBER;
 
 module.exports = () => {
 
+
+  // POST Route to send SMS to restaurent owner
   router.post("/", (req, res) => {
+    // capture message body from submited user form
     let messageText = `
       name: ${req.body.name}
       telephone:${req.body.telephone}
       instruction: ${req.body.description}
     `
     console.log(messageText);
-
-    client.messages
-    .create({
-      to: toNumber,
-      from: fromNumber,
-      body: messageText,
-    })
-    .then((message) => console.log(message.sid));
+    // Uncomment block below to start sending actuall SMSs
+    // client.messages
+    // .create({
+    //   to: toNumber,
+    //   from: fromNumber,
+    //   body: messageText,
+    // })
+    // .then((message) => console.log(message.sid)); //this console logs the msg id of the text message sent to restaurent owner
 
     console.log("message sent");
     res.status(300).send('Your message has been sent successfully');
