cea1a9f9a6c1d91cae7aa53ef24e8903593dd973
VivekPatel3835 <patelvivek3835@gmail.com>
2017-11-26 14:55:15 +0000

    master not up to date with the latest version of menu-items and cart-items even though a merge was completed on Nov 25 @ around 11pm

--- routes/menu_items.js
+++ routes/menu_items.js
@@ -26,16 +26,3 @@ module.exports = (knex) => {
   return router;
 }
 
-// sql query:
-// select order_logs.id from order_logs where order_logs.user_id in
-// (select users.id from users where email in ('alice@doe.com'));
-
-// same query in knex:
-    // let subqueryGetsUserId = knex.select('id').from('users').where('email', req.session.email)
-    // knex.select('id').from('order_logs').where('user_id', subqueryGetsUserId).then((orderNumber) => {
-    //   console.log('myOrderNumber-->', orderNumber)
-    // })
-
-
-
-
